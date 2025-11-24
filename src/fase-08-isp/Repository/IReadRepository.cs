1881f91a52bdb583d32c9a1b7e1972cb5a991b17
Magagnin <magagningu@gmail.com>
2025-11-24 11:11:40 -0300

    Update IReadRepository.cs

--- src/fase-08-isp/Repository/IReadRepository.cs
+++ src/fase-08-isp/Repository/IReadRepository.cs
@@ -1,1 +1,12 @@
+using System.Collections.Generic;
 
+namespace Fase08.Isp.Repository;
+
+/// <summary>
+/// Contrato mínimo de leitura (ISP).
+/// </summary>
+public interface IReadRepository<T, TId>
+{
+    T? GetById(TId id);
+    IReadOnlyList<T> ListAll();
+}
