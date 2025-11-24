0014cbc28f1b3108ed68a5d70046b6b72d6da664
Magagnin <magagningu@gmail.com>
2025-11-24 10:05:40 -0300

    Update Formatter.cs

--- src/fase-04-com-interfaces/Formatter.cs
+++ src/fase-04-com-interfaces/Formatter.cs
@@ -1,1 +1,13 @@
+using System.Globalization;
+
+public static class Formatter
+{
+    public static string FormatDecimal(decimal value, int decimals)
+    {
+        decimal rounded = Math.Round(value, decimals);
+        string formatted = rounded.ToString($"F{decimals}", CultureInfo.InvariantCulture);
+        // remove zeros e ponto quando possível (como em Fase 3)
+        return formatted.TrimEnd('0').TrimEnd('.');
+    }
+}
 
