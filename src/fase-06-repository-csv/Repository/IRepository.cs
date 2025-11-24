a6b5705e6fe42d813ca8d0227461c080cb2f3bf8
Magagnin <magagningu@gmail.com>
2025-11-24 10:33:26 -0300

    Update IRepository.cs

--- src/fase-06-repository-csv/Repository/IRepository.cs
+++ src/fase-06-repository-csv/Repository/IRepository.cs
@@ -1,1 +1,13 @@
+using System.Collections.Generic;
+
+namespace Fase06.RepositoryCsv.Repository;
+
+public interface IRepository<T, TId>
+{
+    T Add(T entity);
+    T? GetById(TId id);
+    IReadOnlyList<T> ListAll();
+    bool Update(T entity);
+    bool Remove(TId id);
+}
 
