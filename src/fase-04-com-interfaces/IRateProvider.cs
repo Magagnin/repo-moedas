ef451add92a7a64100b6f9d1422a5ae2a54ca7ba
Magagnin <magagningu@gmail.com>
2025-11-24 10:03:34 -0300

    Update IRateProvider.cs

--- src/fase-04-com-interfaces/IRateProvider.cs
+++ src/fase-04-com-interfaces/IRateProvider.cs
@@ -1,1 +1,12 @@
+using System.Collections.Generic;
+
+public interface IRateProvider
+{
+    decimal GetRate(string code, string policy);
+    decimal? GetDirectRate(string from, string to);
+    void SetCustomRate(string code, decimal rate);
+    void SetDirectRate(string from, string to, decimal factor);
+    IReadOnlyDictionary<string, decimal> GetBaseRates();
+    IReadOnlyDictionary<string, decimal> GetCustomRates();
+}
 
