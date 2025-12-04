bad79dff3bc6d15905cf9de8ec509fe86c25cc1f
Magagnin <magagningu@gmail.com>
2025-12-03 21:56:03 -0300

    Delete src/fase-09-dubles-async/IAsyncWriter.cs

--- src/fase-09-dubles-async/IAsyncWriter.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace Fase09.DublesAsync.Contracts;
-
-public interface IAsyncWriter<T>
-{
-    Task WriteAsync(T item, CancellationToken ct = default);
-}
